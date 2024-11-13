63d40b665fe3fc83e1ae39b734a92d829d50321e
Ferryj18 <ferryjati18@gmail.com>
2024-11-13 22:16:49 +0700

    Initial Commit

---
+++ WhatsAppCloneTests/WhatsAppCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneTests.swift
+//  WhatsAppCloneTests
+//
+//  Created by Ferry jati on 13/11/24.
+//
+
+import Testing
+@testable import WhatsAppClone
+
+struct WhatsAppCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
